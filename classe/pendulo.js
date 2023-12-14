c85511470bc5de48ed8dca11919bc77c8a57e2b8
Ariel Guerrero <ariel.guerrero@ucuenca.edu.ec>
2023-12-14 12:31:28 -0500

    aaray y forloop agregados

--- classe/pendulo.js
+++ classe/pendulo.js
@@ -1,14 +1,22 @@
 class Pendulo {
-  constructor(x, y, l, a, col) {
+  constructor(x, y, l, da, velocidad, col) {
+    // Definir el punto de origen común y la longitud del péndulo
     this.origin = createVector(x, y);
     this.length = l;
-    this.angle = a;
+    this.dAngular = da; // Desplazamiento angular
+    this.velocidad = velocidad; // Velocidad angular
     this.color = col; // Nuevo parámetro para el color
   }
 
+  update() {
+    this.angle = (Math.PI / 4) * cos(this.dAngular * t * this.velocidad); // Calcular el ángulo en función del tiempo y la velocidad
+    //Math.PI-obtener el valor de la constante pi (π) calculan un ángulo que se utiliza para simular el movimiento de un péndulo
+  }
+
+  // Mostrar el péndulo en la pantalla
   display() {
-    let x = this.length * sin(this.angle);
-    let y = this.length * cos(this.angle);
+    let x = this.length * sin(this.angle); //relación entre el lado opuesto, la coordenada X del extremo del péndulo y la hipotenusa que la longitud del péndulo).
+    let y = this.length * cos(this.angle); //proporciona la relación entre el lado adyacente, la coordenada Y del extremo del péndulo y la hipotenusa que es la longitud del péndulo.
     let position = createVector(x, y);
     position.add(this.origin);
 
@@ -16,7 +24,7 @@ class Pendulo {
     strokeWeight(2);
     line(this.origin.x, this.origin.y, position.x, position.y);
 
-    fill(this.color); // Usar el color proporcionad
+    fill(this.color); // Usar el color proporcionado
     ellipse(position.x, position.y, 20, 20);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,39 @@
 let t = 0; // Tiempo inicial
 let dt = 0.01; // Incremento de tiempo
 
+let pendulos = []; // Array para almacenar los objetos Pendulo
+
 function setup() {
   createCanvas(400, 400);
+
+  // Punto de origen común
+  let x = width / 2;
+  let y = 0;
+
+  // Colores específicos para cada péndulo
+  let colores = [color(148, 0, 211), color(255, 0, 0), color(255, 255, 0)];
+
+  // Crear 3 péndulos con diferentes velocidades y agregarlos al array
+  for (let i = 0; i < 3; i++) {
+    let velocidad = map(i, 0, 3, 1.39, 2.39); // Mapear velocidades de forma manual
+    let longitud = 150;
+    let angulo = 1.39;
+    let col = colores[i];
+
+    pendulos[i] = new Pendulo(x, y, longitud, angulo, velocidad, col);
+  }
 }
 
 function draw() {
   background(255);
 
-  let angle = (Math.PI / 4) * cos(1.39 * t); // Calcular el ángulo en función del tiempo
-
-  let pendulo = new Pendulo(width / 2, 0, 150, angle, color(255, 0, 0));
-  pendulo.display();
+  // Actualizar y mostrar cada péndulo en el array
+  for (let i = 0; i < pendulos.length; i++) {
+    pendulos[i].update();
+    pendulos[i].display();
+  }
 
   t += dt; // Incrementar el tiempo
 }
+
+//   pendulo1 = new Pendulo(width / 2, 0, 150, 1.39, color(255, 0, 0));
